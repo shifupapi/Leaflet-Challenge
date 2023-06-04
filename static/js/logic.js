926925c3f370eaedb7bb23f4a5298022c9d3fb48
Shifupapi <shifupapi@Seans-MacBook-Air.local>
2023-06-04 19:13:03 -0400

    code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 //URL for the GeoJSON data
 let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-// Add a Leaflet tile layer.
+// Leaflet tile layer.
 let streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
@@ -13,9 +13,17 @@ let myMap = L.map("map", {
     layers: [streets]
 });
 
-//define basemaps as the streetmap
+//  Create a basemaps object.
+//     let baseMaps = {
+//         "Satellite": satellite,
+    //     "Grayscale": grayscale,
+    //     "Outdoors": outdoors
+       
+    // }
+//define basemaps as the streetmap 
 let baseMaps = {
     "streets": streets
+
 };
 
 //define the earthquake layergroup and tectonic plate layergroups for the map
@@ -24,8 +32,8 @@ let tectonics = new L.LayerGroup();
 
 //define the overlays and link the layergroups to separate overlays
 let overlays = {
+    "Tectonic Plates": tectonics,
     "Earthquakes": earthquake_data,
-    "Tectonic Plates": tectonics
 };
 
 //add a control layer and pass in baseMaps and overlays
@@ -35,22 +43,17 @@ L.control.layers(baseMaps, overlays).addTo(myMap);
 function styleInfo(feature) {
     return {
         color: chooseColor(feature.geometry.coordinates[2]),
-
-//sets radius based on magnitude 
-        radius: chooseRadius(feature.properties.mag), 
-
-//sets fillColor based on the depth of the earthquake
-        fillColor: chooseColor(feature.geometry.coordinates[2]) 
     }
 };
 
 //define a function to choose the fillColor of the earthquake based on earthquake depth
 function chooseColor(depth) {
-    if (depth <= 10) return "coral";
-    else if (depth > 10 & depth <= 25) return "orange";
-    else if (depth > 25 & depth <= 40) return "blue";
-    else if (depth > 40 & depth <= 55) return "lime";
-    else if (depth > 55 & depth <= 70) return "yellow";
+    if (depth <= 10) return "green";
+    else if (depth > 10 & depth <= 30) return "yellow";
+    else if (depth > 30 & depth <= 50) return "coral";
+    else if (depth > 50 & depth <= 70) return "orange";
+    else if (depth > 70 & depth <= 90) return "blue";
+    else if (depth > 90) return "red";
     else return "purple";
 };
 
@@ -63,7 +66,7 @@ function chooseRadius(magnitude) {
 d3.json(url).then(function (data) { 
     L.geoJson(data, {
         pointToLayer: function (feature, latlon) { 
-//declare a point to layer function that takes a feature and latlon
+
 //function creates a circleMarker at latlon and binds a popup with the earthquake id
             return L.circleMarker(latlon).bindPopup(feature.id);
         },
@@ -74,11 +77,11 @@ d3.json(url).then(function (data) {
     }).addTo(earthquake_data); 
     earthquake_data.addTo(myMap);
 
-//this function pulls the tectonic plate data and draws a red line over the plates
+//this function pulls the tectonic plate data and draws a fuchsia line over the plates
     d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (data) { //pulls tectonic data with d3.json
         L.geoJson(data, {
-//sets the line color to red
-            color: "red", 
+//sets the line color to fuchsia
+            color: "fuchsia", 
             weight: 3
 //add the tectonic data to the tectonic layergroup / overlay
         }).addTo(tectonics); 
@@ -91,39 +94,46 @@ d3.json(url).then(function (data) {
 let legend = L.control({ position: "bottomright" });
 legend.onAdd = function(myMap) {
     var div = L.DomUtil.create("div", "legend");
-    div.innerHTML += "<h4>Depth Color Legend</h4>";
-    div.innerHTML += '<i style="background: red"></i><span>(Depth < 10)</span><br>';
-    div.innerHTML += '<i style="background: orange"></i><span>(10 < Depth <= 25)</span><br>';
-    div.innerHTML += '<i style="background: yellow"></i><span>(25 < Depth <= 40)</span><br>';
-    div.innerHTML += '<i style="background: pink"></i><span>(40 < Depth <= 55)</span><br>';
-    div.innerHTML += '<i style="background: blue"></i><span>(55 < Depth <= 70)</span><br>';
-    div.innerHTML += '<i style="background: green"></i><span>(Depth > 70)</span><br>';
+    // div.innerHTML += "<h4>Depth Color Legend</h4>";
+    div.innerHTML += '<i style="background: green"></i><span>(Depth < 10)</span><br>';
+    div.innerHTML += '<i style="background: yellow"></i><span>(10 < Depth <= 30)</span><br>';
+    div.innerHTML += '<i style="background: coral"></i><span>(30 < Depth <= 50)</span><br>';
+    div.innerHTML += '<i style="background: orange"></i><span>(50 < Depth <= 70)</span><br>';
+    div.innerHTML += '<i style="background: blue"></i><span>(70 < Depth <= 90)</span><br>';
+    div.innerHTML += '<i style="background: red"></i><span>(Depth > 90)</span><br>';
 
  return div;
 };
 //add the legend to the map
 legend.addTo(myMap);
 
-//scratch work for collecting the necessary  and console logging
+//scratch work for collecting the necessary and console logging
 //collect data with d3
 d3.json(url).then(function (data) {
  console.log(data);
  let features = data.features;
  console.log(features);
 
- let results = features.filter(id => id.id == "nc73872510"); //replace the id string with the argument of the function once created
- let first_result = results[0];
+
+let first_result = results[0];
  console.log(first_result);
     let geometry = first_result.geometry;
     console.log(geometry);
     let coordinates = geometry.coordinates;
     console.log(coordinates);
-    console.log(coordinates[0]); // longitude
-    console.log(coordinates[1]); // latitude
-    console.log(coordinates[2]); // depth of earthquake
+
+// longitude
+    console.log(coordinates[0]); 
+
+// latitude
+    console.log(coordinates[1]); 
+
+// depth of earthquake
+    console.log(coordinates[2]); 
     let magnitude = first_result.properties.mag;
     console.log(magnitude);
-    //define depth variable
+
+ //define depth variable
     let depth = geometry.coordinates[2];
     console.log(depth);
     let id = first_result.id;
